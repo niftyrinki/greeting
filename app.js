cb05b9ca09d69c159343bbf6ed7b174fa55c79d0
Rinki Agarwal <niftyrinki@gmail.com>
2016-03-15 12:57:07 +0530

    setup the port and start the server

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+
+app.use(bodyParser.urlencoded({ extended : true}));
+app.use(bodyParser.json());
+
+// set your port
+
+var port = process.env.PORT || 8800 ;
+
+module.exports = app;
+
+// start the server 
+
+app.listen(port);
+
+console.log("Start the server : "+ port );	
